3a3fa82dafe32a7f77ba4ec461e4c37a838bcb47
Sergio Somma <sdsomma@gmail.com>
2019-07-27 14:45:06 -0700

    Removing old tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('go-figure');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Go Figure');
-  });
 });
