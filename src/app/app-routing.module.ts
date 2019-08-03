f532f172d8559d69dabbed7e82851bf2a8e65bad
Jan Hartigan <janhartigan13@gmail.com>
2019-08-03 16:50:08 -0700

    fixing route scrolling issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
