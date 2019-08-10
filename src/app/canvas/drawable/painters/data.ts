b2a6968aa493e072e6c51c74c4b1e5fa0d3afb58
Sergio Somma <sdsomma@gmail.com>
2019-08-10 01:12:52 -0700

    Fixing draw undefined prevPoint bug

--- src/app/canvas/drawable/painters/data.ts
+++ src/app/canvas/drawable/painters/data.ts
@@ -18,21 +18,30 @@ export class DataPainter {
     this.canvasManager.setLineWidth(this.lineWidth);
 
     data.forEach((point, i) => {
-      if (i == data.length - 1 && animation.centering) {
+      if (prevPoint) {
+        if (this.shouldAnimate(animation, data, i)) {
+          let endPoint = this.getAnimationEndPoint(animation, prevPoint, point);
 
-        let distance: number = (new Vector(prevPoint.toPoint2D(), point.toPoint2D())).length(),
-            deltaX = (point.x - prevPoint.x) / animation.totalSteps * animation.currentStep,
-            deltaY = (point.y - prevPoint.y) / animation.totalSteps * animation.currentStep,
-            partialPoint = new Point2D(prevPoint.x + deltaX, prevPoint.y + deltaY);
-
-        this.canvasManager.paintLine(prevPoint.toPoint2D(), partialPoint);
-      }
-      else if (i != 0) {
-        this.canvasManager.paintLine(prevPoint.toPoint2D(), point.toPoint2D());
+          this.canvasManager.paintLine(prevPoint.toPoint2D(), endPoint);
+        }
+        else
+          this.canvasManager.paintLine(prevPoint.toPoint2D(), point.toPoint2D());
       }
 
       prevPoint = point;
     })
+  }
+
+  protected shouldAnimate(animation: AnimationConfig, data: Point3D[], index: number): boolean {
+    return index == data.length - 1 && animation.centering;
+  }
+
+  protected getAnimationEndPoint(animation: AnimationConfig, fromPoint: Point3D, toPoint: Point3D): Point2D {
+    let distance = (new Vector(fromPoint.toPoint2D(), toPoint.toPoint2D())).length(),
+        deltaX = (toPoint.x - fromPoint.x) / animation.totalSteps * animation.currentStep,
+        deltaY = (toPoint.y - fromPoint.y) / animation.totalSteps * animation.currentStep,
+
+    return new Point2D(fromPoint.x + deltaX, fromPoint.y + deltaY);
   }
 
 }
