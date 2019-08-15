1765ddf6dadd06c428e8101ac606520dbf5888ad
Sergio Somma <sdsomma@gmail.com>
2019-08-15 01:40:27 -0700

    add gesture patch

---
+++ src/app/gesture-config.ts
@@ -0,0 +1,14 @@
+import * as hammer from 'hammerjs';
+import { GestureConfig, HammerInstance } from '@angular/material';
+
+export class PatchedGestureConfig extends GestureConfig {
+
+  buildHammer (element: HTMLElement): HammerInstance {
+    const mc = <HammerManager> super.buildHammer(element);
+    const pinch = new hammer.Pinch();
+
+    mc.add(pinch);
+
+    return <HammerInstance> mc;
+  }
+}
